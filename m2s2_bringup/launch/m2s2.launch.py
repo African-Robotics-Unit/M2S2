3d374077908d3df2a781e3adace11fa5b768f141
lindelanimcebo <lindelanimcebo@gmail.com>
2022-08-18 19:14:42 +0200

    single bag file functionality

--- m2s2_bringup/launch/m2s2.launch.py
+++ m2s2_bringup/launch/m2s2.launch.py
@@ -4,12 +4,51 @@ from ament_index_python import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.launch_description_sources import AnyLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription
-
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration, PythonExpression
 
 def generate_launch_description():
-    ld = LaunchDescription()
+
     # ARGS
+    bag_name = LaunchConfiguration("bag_name")
+    image_topic = LaunchConfiguration("image_topic")
+    image_info_topic = LaunchConfiguration("image_info_topic")
+    audio_topic = LaunchConfiguration("audio_topic")
+    enviro_topic = LaunchConfiguration("enviro_topic")
+    
+    bag_name_arg = DeclareLaunchArgument(
+        'bag_name',
+        default_value='bags/test'
+    )
+    
+    image_topic_arg = DeclareLaunchArgument(
+        'image_topic',
+        default_value='/image_raw'
+    )
+
+    image_info_topic_arg = DeclareLaunchArgument(
+        'image_info_topic',
+        default_value='camera_info'
+    )
+
+    audio_topic_args = DeclareLaunchArgument(
+        'audio_topic',
+        default_value='audio/audio'
+    )
+
+    enviro_topic_arg = DeclareLaunchArgument(
+        'enviro_topic',
+        default_value='enviro_data'
+    )
+
+    ld = LaunchDescription([
+        bag_name_arg,
+        image_topic_arg,
+        image_info_topic_arg,
+        audio_topic_args,
+        enviro_topic_arg
+    ])
+
 
     # PACKAGES: include launch files from other packages
 
@@ -45,8 +84,24 @@ def generate_launch_description():
                 'launch/xiCam.launch.xml'))
     )
 
-    # ld.add_action(audio_capture_to_file_launch)
+    # Record all topics at once
+
+    record_all_topics_action = ExecuteProcess(
+            cmd=[
+                'ros2',
+                'bag',
+                'record',
+                image_topic,
+                image_info_topic,
+                audio_topic,
+                enviro_topic , '-o',
+                bag_name],
+            output='screen'
+    )
+
+    # Add Nodes
     ld.add_action(audio_capture_to_bag_launch)
     ld.add_action(enviro_capture_to_bag_launch)
     ld.add_action(ximea_capture_to_bag_launch)
+    ld.add_action(record_all_topics_action)
     return ld 
---
+++ m2s2_bringup/launch/m2s2_test.launch.py
@@ -0,0 +1,26 @@
+from launch_ros.substitutions import FindPackageShare
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('m2s2_bringup'),
+                    'launch/m2s2.launch.py'
+                ])
+            ]),
+            launch_arguments={
+                'bag_name': 'bags/test',
+                'image_topic': '/image_raw',
+                'image_info_topic': '/camera_info',
+                'audio_topic': '/audio/audio',
+                'enviro_topic': '/enviro_data'
+            }.items()
+        )
+    ])
