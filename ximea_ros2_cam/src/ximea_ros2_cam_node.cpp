2e6974adddbd4f2bac554e1135aa40b850ad8faf
DenDen047 <sh.mn.nat@gmail.com>
2022-08-29 13:09:17 +0200

    update the indent

--- ximea_ros2_cam/src/ximea_ros2_cam_node.cpp
+++ ximea_ros2_cam/src/ximea_ros2_cam_node.cpp
@@ -2,9 +2,9 @@
 
 int main(int argc, char ** argv)
 {
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<ximea_ros2_cam::XimeaROSCam>();
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<ximea_ros2_cam::XimeaROSCam>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
